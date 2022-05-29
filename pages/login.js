9322a5e2f28704f6ce6a4e71419a5e5283a81493
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-05-29 15:00:34 +0300

    js login// Calling createAccount function

--- pages/login.js
+++ pages/login.js
@@ -5,3 +5,10 @@ let create_account=document.getElementById("login-create-account");
 let inputs=[email,password];
 let email_condition = false;
 let password_condtion = false;
+
+// Call the createAccount function upon clicking the create_account button
+create_account.onclick=createAccount;
+
+function createAccount(){
+    
+}
