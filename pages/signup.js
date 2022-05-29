13fd6c054e385c6e9c8f671368f05f4762f150e8
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-05-29 16:15:54 +0300

    js signup// Declaring the required variables

---
+++ pages/signup.js
@@ -0,0 +1,9 @@
+// Declaring the necessary variables
+let username=document.getElementById("signup-username-input");
+let email=document.getElementById("signup-email-input");
+let password=document.getElementById("signup-password-input");
+let create_account=document.getElementById("signup-create-account");
+let inputs=[username,email,password];
+
+let email_condition = false;
+let password_condtion = false;
