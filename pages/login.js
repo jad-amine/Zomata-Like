4cdf332151b86843f38683b5622d661d355343b6
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-05-29 15:04:48 +0300

    js login// Checking if email & password are filled correctly

--- pages/login.js
+++ pages/login.js
@@ -10,5 +10,28 @@ let password_condtion = false;
 create_account.onclick=createAccount;
 
 function createAccount(){
-    
-}
+
+    //// If all fields are filled 
+  if (email.value != "" && password.value != ""){
+
+    //--- To check the email format if valid or no
+    var mail_format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(email.value.match(mail_format)){
+      email_condition=true;
+    }
+    else{
+      email.style.borderBlockColor="red";
+      alert("Enter a valid email address! (example@gmail.com)");
+    }
+
+    //--- To check the password format if strong or no
+    var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    if(password.value.match(passw)){ 
+      password_condtion=true;
+    }
+    else{ 
+      password.style.borderBlockColor="red";
+      alert('Password must be between 6-20 characters, containing at least one numeric digit, one uppercase and one lowercase letter!')
+    }
+    }
+}    
