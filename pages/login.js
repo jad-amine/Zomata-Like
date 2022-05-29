ab0a0057f7914796a2c569ad9633ae8e58bc5a6a
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-05-29 14:58:24 +0300

    js login// Declaring needed variables

---
+++ pages/login.js
@@ -0,0 +1,7 @@
+// Declaring the necessary variables
+let email=document.getElementById("login-email-input");
+let password=document.getElementById("login-password-input");
+let create_account=document.getElementById("login-create-account");
+let inputs=[email,password];
+let email_condition = false;
+let password_condtion = false;
