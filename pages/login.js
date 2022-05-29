064b0d6440eae0b2eb140ae26a0a0efa2e07cc9b
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-05-29 15:19:37 +0300

    js login// Fixing the name of the login button

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,13 @@
 // Declaring the necessary variables
 let email=document.getElementById("login-email-input");
 let password=document.getElementById("login-password-input");
-let create_account=document.getElementById("login-create-account");
+let log_in=document.getElementById("login-create-account");
 let inputs=[email,password];
 let email_condition = false;
 let password_condtion = false;
 
-// Call the createAccount function upon clicking the create_account button
-create_account.onclick=logIn;
+// Call the logIn function upon clicking the log_in button
+log_in.onclick=logIn;
 
 function logIn(){
 
