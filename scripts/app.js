e53f69c8fe0e84c1c07b04c023651b53f9c83d41
jad amine <jade1992amine@gmail.com>
2022-05-30 22:55:54 +0300

    Add new restaurant functionality

--- scripts/app.js
+++ scripts/app.js
@@ -2,5 +2,5 @@ let name1 = localStorage.getItem('name');
 sign_up_bttn = document.querySelector("#signup-btn");
 
 if(name1){
-  sign_up_bttn.innerText = 'welcome ' +name1;
-}
+  sign_up_bttn.innerText = 'welcome ' + name1;
+};
