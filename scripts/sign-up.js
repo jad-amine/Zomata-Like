44e132bb1dcfbb9c4032c48dbf15895556ac3c69
jad amine <jade1992amine@gmail.com>
2022-05-30 18:08:29 +0300

    Linked login Back-Front end

--- scripts/sign-up.js
+++ scripts/sign-up.js
@@ -6,15 +6,16 @@ function send_data(){
   var name =document.querySelector("#username");
   var email =document.querySelector("#email");
   var pass =document.querySelector("#pass");
-  let data = {name: `${name.value}`, email:  `${email.value}`, pass:`${pass.value}`};
-  axios({
-    method: 'post',
-    url: 'http://localhost/Zomato/Zomato_Back-End/sign_up.php',
-    data: data,
-  })
-  .then(function (response) {
-    console.log(response);
-    }
-  )
-}
+  let data = new FormData();
+  data.append('name', `${name.value}`);
+  data.append('email',  `${email.value}`);
+  data.append('pass', `${pass.value}`);
 
+  axios.post('http://localhost/Zomato/Zomato_Back-End/sign_up.php', 
+    data)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch((e) => console.log(e));
+    
+};
