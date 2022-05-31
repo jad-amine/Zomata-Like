7db094781bafb7fdc93f2d095096aaa0966b37b5
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-05-31 08:28:48 +0300

    Fixing selection in add-resto html + Declaring variables in main js

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,12 @@ sign_up_bttn = document.querySelector("#signup-btn");
 if(name1){
   sign_up_bttn.innerText = 'welcome ' + name1;
 };
+
+let italian = document.getElementById("italian-food")
+let sea = document.getElementById("sea-food")
+let pizza = document.getElementById("pizza-food")
+let burger = document.getElementById("burger-food")
+let oriental = document.getElementById("oriental-food")
+let dessert = document.getElementById("deserts-food")
+
+
