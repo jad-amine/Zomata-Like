d0aab35f6e831c46f2245c96284bb6a71f1ac4bf
jad amine <jade1992amine@gmail.com>
2022-05-31 10:22:19 +0300

    Grabed all restaurants by category from Backend

--- scripts/category.js
+++ scripts/category.js
@@ -8,8 +8,22 @@ category = category.category;
 document.querySelector("#category-name").innerHTML = category;;
 let url = 'http://localhost/ZOMATO/Zomato_Back-End/get_restaurants_by_category.php?category=' + category;
 console.log(url);
-axios.get(url).then(res => console.log(res.data));
 
-// function display(data){
+axios.get(url)
+  .then(res => { populate(res) })
+  .catch(e=> console.log(e));
 
-// }
+
+function populate(res){
+  let restaurants = res.data;
+  let section = document.querySelector(".restaurants-categories");
+  for (let i = 0; i < restaurants.length; i++){
+    let name = restaurants[i].restaurant_name;
+    // console.log(restaurants[i].restaurant_name);
+    
+    // <div class="restaurants">
+    //   <div><p>${name}</p><img class="resto" src="../public/mc.jfif" alt=""></div>
+    //   <div><p>Roadster</p><img class="resto" src="../public/roadster.jfif" alt=""></div>
+    //   </div>
+  }
+}
